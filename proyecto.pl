1446123d44593a63113e2fa054076896bf02f205
karenp.bmedina@unitec.edu <karenp.bmedina@unitec.edu>
2018-08-21 15:19:32 -0600

    estudiantes

---
+++ proyecto.pl
@@ -0,0 +1,68 @@
+estudiante(2000100001,1,[math101, phys201, ec201]) .
+estudiante(2000100002,1,[math101, phys201, hist301]).
+estudiante(2000100003,1,[math101, ec201, hist301]) .
+estudiante(2000100004,1,[math101, phys201, ec201, hist301]).
+estudiante(2000100007,0,[ec201, phys201]).
+estudiante(2000100015,0,[ec201, hist301]) .
+estudiante(2000100016,0,[cmpe150, hist301]).
+estudiante(2000100017,0,[]).
+estudiante(2000100008,0,[math101, phys201]) .
+estudiante(2000100009,1,[phys201, ec201]).
+estudiante(2000100010,0,[cmpe150, math101]).
+estudiante(2000100011,0,[cmpe150, hist301]).
+estudiante(2000100012,1,[math101, phys201]).
+estudiante(2002100009,0,[math101, ec201, hist301]).
+estudiante(2002100071,0,[phys201, ec201, hist301]).
+estudiante(2002100053,0,[cmpe150, ec201]).
+estudiante(2002100040,1,[math101, hist301]).
+estudiante(2002100032,0,[phys201, ec201]).
+estudiante(2002100010,0,[cmpe150]).
+estudiante(2002103010,1,[]).
+estudiante(2000100101,1,[math101, phys201, ec201]) .
+estudiante(2000100202,1,[math101, phys201, hist301]).
+estudiante(2000100303,1,[math101, ec201, hist301]) .
+estudiante(2000100404,1,[math101, phys201, ec201, hist301]).
+estudiante(2000100507,0,[ec201, phys201]).
+estudiante(2000100615,0,[ec201, hist301]) .
+estudiante(2000100716,0,[cmpe150, hist301]).
+estudiante(2000100817,0,[]).
+estudiante(2000100908,0,[math101, phys201]) .
+estudiante(2000100109,1,[phys201, ec201]).
+estudiante(2000100210,0,[cmpe150, math101]).
+estudiante(2000100311,0,[cmpe150, hist301]).
+estudiante(2000100412,1,[math101, phys201]).
+estudiante(2002100509,0,[math101, ec201, hist301]).
+estudiante(2002100671,0,[phys201, ec201, hist301]).
+estudiante(2002100753,0,[cmpe150, ec201]).
+estudiante(2002100840,1,[math101, hist301]).
+estudiante(2002100932,0,[phys201, ec201]).
+estudiante(2002101010,0,[cmpe150]).
+estudiante(2002103510,1,[]).
+estudiante(2000109101,1,[math101, phys201, ec201]) .
+estudiante(2000108202,1,[math101, phys201, hist301]).
+estudiante(2000107303,1,[math101, ec201, hist301]) .
+estudiante(2000106404,1,[math101, phys201, ec201, hist301]).
+estudiante(2000105507,0,[ec201, phys201]).
+estudiante(2000104615,0,[ec201, hist301]) .
+estudiante(2000103716,0,[cmpe150, hist301]).
+estudiante(2000102817,0,[]).
+estudiante(2000101908,0,[math101, phys201]) .
+estudiante(2000102109,1,[phys201, ec201]).
+estudiante(2000103210,0,[cmpe150, math101]).
+estudiante(2000104311,0,[cmpe150, hist301]).
+estudiante(2000105412,1,[math101, phys201]).
+estudiante(2002106509,0,[math101, ec201, hist301]).
+estudiante(2002107671,0,[phys201, ec201, hist301]).
+estudiante(2002108753,0,[cmpe150, ec201]).
+estudiante(2002109840,1,[math101, hist301]).
+estudiante(2002109932,0,[phys201, ec201]).
+estudiante(2002108010,0,[cmpe150]).
+estudiante(2002173510,1,[]).
+estudiante(2000101958,0,[math101, phys201]) .
+estudiante(2000102139,1,[phys201, ec201]).
+estudiante(2000103215,0,[cmpe150, math101]).
+estudiante(2000104311,0,[cmpe150, hist301]).
+estudiante(2000105412,1,[math101, phys201]).
+estudiante(2002196509,0,[math101, hist301]).
+
+
