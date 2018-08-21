097f20731d3f763c4d058fc401d2b076e911db61
canozo <canwoahzo@gmail.com>
2018-08-21 15:22:19 -0600

    Aulas

--- proyecto.pl
+++ proyecto.pl
@@ -1,3 +1,4 @@
+# Base de estudiantes
 estudiante(2000100001,1,[math101, phys201, ec201]) .
 estudiante(2000100002,1,[math101, phys201, hist301]).
 estudiante(2000100003,1,[math101, ec201, hist301]) .
@@ -65,4 +66,71 @@ estudiante(2000104311,0,[cmpe150, hist301]).
 estudiante(2000105412,1,[math101, phys201]).
 estudiante(2002196509,0,[math101, hist301]).
 
-
+# Base de aulas
+aula(nh101, 50, 10).
+aula(nh201, 50, 10).
+aula(nh301, 50, 10).
+aula(m2001, 30, 5).
+aula(etaa2, 30, 5).
+aula(etaa3, 35, 5).
+aula(etaa4, 30, 10).
+aula(etaa5, 20, 5).
+aula(etaa6, 30, 5).
+aula(etaa7, 25, 10).
+aula(etaa8, 30, 15).
+aula(etaa9, 35, 5).
+aula(etb101, 20, 5).
+aula(etb201, 45, 5).
+aula(etb301, 56, 5).
+aula(etb401, 67, 5).
+aula(etb501, 28, 5).
+aula(etb601, 23, 10).
+aula(etb701, 44, 5).
+aula(etb801, 55, 5).
+aula(etb901, 26, 10).
+aula(ppd101, 20, 5).
+aula(ppd201, 30, 5).
+aula(ppd301, 40, 15).
+aula(ppd401, 50, 15).
+aula(ppd501, 60, 5).
+aula(ppd601, 70, 15).
+aula(ppd701, 80, 15).
+aula(ppd801, 35, 5).
+aula(ppd901, 25, 10).
+aula(wwe101, 21, 5).
+aula(wwe201, 31, 5).
+aula(wwe301, 21, 15).
+aula(wwe401, 41, 5).
+aula(wwe501, 51, 5).
+aula(wwe601, 21, 15).
+aula(wwe701, 51, 5).
+aula(wwe801, 21, 5).
+aula(wwe901, 21, 5).
+aula(hkd101, 30, 10).
+aula(hkd201, 40, 20).
+aula(hkd301, 50, 5).
+aula(hkd401, 25, 5).
+aula(hkd501, 25, 10).
+aula(hkd601, 15, 20).
+aula(hkd701, 30, 10).
+aula(hkd801, 45, 15).
+aula(hkd901, 50, 10).
+aula(ppmd101, 20, 5).
+aula(ppmd201, 30, 5).
+aula(ppmd301, 20, 5).
+aula(ppmd401, 20, 5).
+aula(ppmd501, 20, 5).
+aula(ppmd601, 30, 5).
+aula(ppmd701, 40, 10).
+aula(ppmd801, 50, 15).
+aula(ppmd901, 35, 5).
+aula(dtf101, 25, 5).
+aula(dtf102, 25, 5).
+aula(dtf201, 35, 5).
+aula(dtf301, 45, 15).
+aula(dtf401, 20, 5).
+aula(dtf501, 20, 5).
+aula(dtf601, 30, 5).
+aula(dtf701, 40, 10).
+aula(dtf801, 50, 15).
+aula(dtf901, 20, 5).
